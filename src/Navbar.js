272dcc33be6de40aff0442b2e94e224093a36db9
sahilhusain0330-hash <sahilhusain788@gmail.com>
2025-08-20 00:45:32 +0530

    19/8 first

---
+++ src/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BiAnchor } from "react-icons/bi";
+function Navbar() {
+  return (
+    <div>Navbar
+      <BiAnchor />
+
+    <h1>Hello world!</h1>
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/Sidebar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { BiAlignLeft } from "react-icons/bi";
+
+export default function Sidebar() {
+  return (
+    <div>Sidebar
+      <BiAlignLeft />
+      </div>
+  )
+}
